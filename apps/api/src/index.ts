f06d23817b5887ae8e1fc4fccc832769bc1cda86
Skalv <fboutin76@gmail.com>
2025-06-10 14:55:51 +0200

    Create API with wrangler

---
+++ apps/api/src/index.ts
@@ -0,0 +1,104 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import { Hono } from "hono";
+import { cache } from "hono/cache";
+import { cors } from "hono/cors";
+
+type Bindings = {
+	DB: D1Database,
+	CACHE: KVNamespace,
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+// CORS
+app.use("*", cors({
+	origin: ['http://localhost:5173'],
+	allowHeaders: ['Content-Type'],
+	allowMethods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
+
+// Static CACHE
+app.use('/tokens/*', cache({
+	cacheName: 'winnieswap',
+	cacheControl: 'max-age-300' // 5 Minutes
+}))
+
+// Main route
+app.use('/tokens/:network?', async (c) => {
+	const chainId = c.req.param('network') === "bepolia" ? "80069" : "80094"
+
+	try {
+		const cachedTokens = await c.env.CACHE.get(`tokens:${chainId}`)
+		if (cachedTokens) {
+			return c.json(JSON.parse(cachedTokens))
+		}
+
+		const { results } = await c.env.DB.prepare(
+			'SELECT * FROM tokens WHERE chain_id = ? ORDER BY created_at DESC'
+		).bind(chainId).all()
+
+		await c.env.CACHE.put(
+			`tokens:${chainId}`, JSON.stringify(results), { expirationTtl: 300 }
+		)
+
+		return c.json(results)
+	} catch (error) {
+		return c.json({ error: "Failed to fetch tokens" }, 500)
+	}
+})
+
+// Sync tokens with berachain metadatas
+const fetchGithub = async (c) => {
+	const networks = ['bepolia', 'mainnet']
+
+	for (const network of networks) {
+		try {
+			const response = await fetch(
+				`https://raw.githubusercontent.com/berachain/metadata/main/src/tokens/${network}.json`,
+			)
+
+			if (!response.ok) continue
+
+			const githubTokens = await response.json()
+
+			for (const token of githubTokens.tokens) {
+				await c.env.DB.prepare(`
+          INSERT OR REPLACE INTO tokens
+          (symbol, name, address, decimals, chain_id, logo_uri, is_verified, coingecko_id, source, updated_at)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'github', CURRENT_TIMESTAMP)
+        `).bind(
+					token.symbol,
+					token.name || "",
+					token.address,
+					token.decimals || 18,
+					token.chainId,
+					token.logoURI || null,
+					true,
+					token?.extensions?.coingeckoId || null
+				).run()
+			}
+
+			await c.env.CACHE.delete(`tokens:80094`)
+			await c.env.CACHE.delete('tokens:80069')
+
+		} catch (error) {
+			console.error(`Sync failed for ${network}:`, error)
+		}
+	}
+}
+
+export default {
+	fetch: app.fetch,
+} satisfies ExportedHandler<Env>;
