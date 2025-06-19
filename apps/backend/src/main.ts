877efefc6296459716a3a7fcc15919db807882f4
Skalv <fboutin76@gmail.com>
2025-06-19 09:48:32 +0200

    Enable CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const allowedOrigins = ['https://winnieswap.com', 'http://localhost:5173'];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: allowedOrigins,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
