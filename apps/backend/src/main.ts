5a04d15d2cabdd8edd25f100934326d9b38c67e6
Skalv <fboutin76@gmail.com>
2025-06-19 16:48:16 +0200

    Allow CORS from Cloudflare pages

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const allowedOrigins = ['https://winnieswap.com', 'http://localhost:5173'];
+const allowedOrigins = [
+  'https://winnieswap.com',
+  'http://localhost:5173',
+  /\.winnieswap\.pages\.dev$/,
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
