d5d16285b5136313f6467bc23031b93fc0aa086d
Skalv <fboutin76@gmail.com>
2025-07-21 16:43:32 +0200

    Create reindex function

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Token } from '@repo/db';
 import { DatabaseService } from './database/database.service';
+import { FallbackIndexerService } from './tracker/fallbackIndexer.service';
+import { start } from 'repl';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly db: DatabaseService,
+    private readonly indexer: FallbackIndexerService,
   ) {}
 
   @Get()
@@ -42,5 +45,10 @@ export class AppController {
       inPool: _count.poolsAsToken0 > 0 || _count.poolsAsToken1 > 0,
       lastPrice: Statistic.length > 0 ? Statistic[0].price : 0,
     }));
+  }
+
+  @Get('/reindex/:startBlock')
+  async reindex(@Param('startBlock') startBlock: string): Promise<void> {
+    await this.indexer.reIndex(BigInt(startBlock));
   }
 }
