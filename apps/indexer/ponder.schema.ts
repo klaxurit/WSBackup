b65dc3206796bbdedab3bd6824c97138e309b0c2
Skalv <fboutin76@gmail.com>
2025-08-06 16:04:23 +0200

    Setup indexing data

--- apps/indexer/ponder.schema.ts
+++ apps/indexer/ponder.schema.ts
@@ -1,17 +1,20 @@
-import { onchainTable, relations } from "ponder";
+import { onchainEnum, onchainTable, primaryKey, relations } from "ponder";
 
 export const pools = onchainTable("i_pools", (t) => ({
   address: t.text().primaryKey(),
   token0: t.text().notNull(),
   token1: t.text().notNull(),
+  tickSpacing: t.integer(),
   fee: t.integer(),
   tvlUSD: t.real().default(0),
   volume24h: t.real().default(0),
-  createdAt: t.timestamp().notNull(),
+  createdAt: t.bigint().notNull(),
   createdAtBlock: t.bigint().notNull(),
 }));
 export const poolsRelations = relations(pools, ({ many }) => ({
-  swaps: many(swaps)
+  swaps: many(swaps),
+  positions: many(positions),
+  liquidityEvent: many(liquidityEvent)
 }))
 
 export const swaps = onchainTable("i_swaps", (t) => ({
@@ -21,10 +24,47 @@ export const swaps = onchainTable("i_swaps", (t) => ({
   recipient: t.text().notNull(),
   amount0: t.bigint().notNull(),
   amount1: t.bigint().notNull(),
-  timestamp: t.timestamp().notNull(),
+  sqrtPriceX96: t.bigint().notNull(),
+  liquidity: t.bigint().notNull(),
+  tick: t.integer().notNull(),
+  timestamp: t.bigint().notNull(),
   blockNumber: t.bigint().notNull(),
   transactionHash: t.text().notNull(),
 }));
 export const swapsRelations = relations(swaps, ({ one }) => ({
   pool: one(pools, { fields: [swaps.poolAddress], references: [pools.address] })
+}))
+
+export const positions = onchainTable("i_positions", (t) => ({
+  poolAddress: t.text(),
+  owner: t.text().notNull(),
+  tickLower: t.integer().notNull(),
+  tickUpper: t.integer().notNull(),
+  liquidity: t.bigint().notNull(),
+  amount0: t.bigint().notNull(),
+  amount1: t.bigint().notNull(),
+  createdAt: t.bigint().notNull(),
+  updatedAt: t.bigint().notNull()
+}), (table) => ({ pk: primaryKey({ columns: [table.poolAddress, table.owner, table.tickLower, table.tickUpper] }) }))
+export const positionsRelations = relations(positions, ({ one }) => ({
+  pool: one(pools, { fields: [positions.poolAddress], references: [pools.address] })
+}))
+
+export const liquidityEventTypeEnum = onchainEnum("type", ["MINT", "BURN"])
+export const liquidityEvent = onchainTable("i_liquidityEvents", (t) => ({
+  id: t.text().primaryKey(),
+  poolAddress: t.text(),
+  owner: t.text().notNull(),
+  type: liquidityEventTypeEnum("type"),
+  tickLower: t.integer().notNull(),
+  tickUpper: t.integer().notNull(),
+  amount: t.bigint().notNull(),
+  amount0: t.bigint().notNull(),
+  amount1: t.bigint().notNull(),
+  timestamp: t.bigint().notNull(),
+  blockNumber: t.bigint().notNull(),
+  transactionHash: t.text().notNull(),
+}))
+export const liquidityEventRelations = relations(liquidityEvent, ({ one }) => ({
+  pool: one(pools, { fields: [liquidityEvent.poolAddress], references: [pools.address] })
 }))
--- apps/indexer/src/index.ts
+++ apps/indexer/src/index.ts
@@ -1,15 +1,16 @@
 import { ponder } from "ponder:registry";
-import { pools, swaps } from "ponder:schema";
+import { liquidityEvent, liquidityEventTypeEnum, pools, positions, swaps } from "ponder:schema";
 
 ponder.on("WinnieFactory:PoolCreated", async ({ event, context }) => {
-  const { token0, token1, pool, fee } = event.args;
+  const { token0, token1, pool, fee, tickSpacing } = event.args;
 
   await context.db.insert(pools).values({
     address: pool,
     token0: token0.toLowerCase(),
     token1: token1.toLowerCase(),
     fee: Number(fee),
-    createdAt: new Date(Number(event.block.timestamp) * 1000),
+    tickSpacing: Number(tickSpacing),
+    createdAt: event.block.timestamp,
     createdAtBlock: event.block.number
   })
 })
@@ -24,8 +25,80 @@ ponder.on("WinniePool:Swap", async ({ event, context }) => {
     recipient: recipient.toLowerCase(),
     amount0,
     amount1,
-    timestamp: new Date(Number(event.block.timestamp) * 1000),
+    sqrtPriceX96,
+    liquidity,
+    tick,
+    timestamp: event.block.timestamp,
     blockNumber: event.block.number,
     transactionHash: event.transaction.hash,
   });
 });
+
+ponder.on("WinniePool:Mint", async ({ event, context }) => {
+  const { owner, tickLower, tickUpper, amount, amount0, amount1 } = event.args
+
+  await context.db.insert(liquidityEvent).values({
+    id: `${event.transaction.hash}-${event.log.logIndex}`,
+    poolAddress: event.log.address.toLowerCase(),
+    owner,
+    type: "MINT",
+    tickLower,
+    tickUpper,
+    amount,
+    amount0,
+    amount1,
+    timestamp: event.block.timestamp,
+    blockNumber: event.block.number,
+    transactionHash: event.transaction.hash
+  })
+
+  await context.db.insert(positions).values({
+    poolAddress: event.log.address.toLowerCase(),
+    owner,
+    tickLower,
+    tickUpper,
+    liquidity: amount,
+    amount0,
+    amount1,
+    createdAt: event.block.timestamp,
+    updatedAt: event.block.timestamp
+  }).onConflictDoUpdate((row) => ({
+    liquidity: row.liquidity + amount,
+    amount0: row.amount0 + amount0,
+    amount1: row.amount1 + amount1,
+    updatedAt: event.block.timestamp
+  }))
+})
+
+ponder.on("WinniePool:Burn", async ({ event, context }) => {
+  const { owner, tickLower, tickUpper, amount, amount0, amount1 } = event.args
+
+  await context.db.insert(liquidityEvent).values({
+    id: `${event.transaction.hash}-${event.log.logIndex}`,
+    poolAddress: event.log.address.toLowerCase(),
+    owner,
+    type: "BURN",
+    tickLower,
+    tickUpper,
+    amount,
+    amount0,
+    amount1,
+    timestamp: event.block.timestamp,
+    blockNumber: event.block.number,
+    transactionHash: event.transaction.hash
+  })
+
+  await context.db.update(positions, {
+    poolAddress: event.log.address.toLowerCase(),
+    owner,
+    tickLower,
+    tickUpper
+  }).set((row) => ({
+    liquidity: row.liquidity - amount,
+    amount0: row.amount0 - amount0,
+    amount1: row.amount1 - amount1,
+    updatedAt: event.block.timestamp
+  }))
+})
+
+
