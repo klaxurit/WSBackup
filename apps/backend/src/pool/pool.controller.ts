3937c5f858f39e53b0276b81e299d92c02d7e4b2
Skalv <fboutin76@gmail.com>
2025-08-14 11:13:10 +0200

    Update pool controller

--- apps/backend/src/pool/pool.controller.ts
+++ apps/backend/src/pool/pool.controller.ts
@@ -6,72 +6,16 @@ import {
   UseInterceptors,
   Get,
 } from '@nestjs/common';
-import { Prisma, TokenState } from '@repo/db';
+import { Prisma } from '@repo/db';
 import { DatabaseService } from 'src/database/database.service';
 
-@Controller('token')
+@Controller('pools')
 @UseInterceptors(CacheInterceptor)
 export class PoolController {
   constructor(private readonly db: DatabaseService) {}
 
   @Get('/')
-  @CacheKey('token:list')
-  @CacheTTL(5 * 60 * 1000) // 5 minutes
-  async getAllTokens(
-    @Query('currentPage', new ParseIntPipe({ optional: true }))
-    currentPage: number = 1,
-    @Query('itemByPage', new ParseIntPipe({ optional: true }))
-    itemByPage: number = 50,
-    @Query('searchValue') searchValue?: string,
-  ) {
-    const page = Math.max(1, currentPage);
-    const limit = Math.min(Math.max(1, itemByPage), 1000);
-    const skip = (page - 1) * limit;
-
-    const searchFilter = searchValue
-      ? {
-          OR: [
-            {
-              name: {
-                contains: searchValue,
-                mode: Prisma.QueryMode.insensitive,
-              },
-            },
-            {
-              symbol: {
-                contains: searchValue,
-                mode: Prisma.QueryMode.insensitive,
-              },
-            },
-          ],
-        }
-      : {};
-
-    const count = await this.db.token.count({ where: searchFilter });
-    const tokens = await this.db.token.findMany({
-      where: searchFilter,
-      take: limit,
-      skip: skip,
-    });
-    const totalPages = Math.ceil(count / limit);
-    const hasNextPage = page < totalPages;
-    const hasPreviousPage = page > 1;
-
-    return {
-      data: tokens,
-      pagination: {
-        currentPage: page,
-        itemsPerPage: limit,
-        totalItems: count,
-        totalPages: totalPages,
-        hasNextPage: hasNextPage,
-        hasPreviousPage: hasPreviousPage,
-      },
-    };
-  }
-
-  @Get('/stats')
-  @CacheKey('token:stats')
+  @CacheKey('pool:stats')
   @CacheTTL(5 * 60 * 1000) // 5 minutes
   async getAllTokensWithStats(
     @Query('currentPage', new ParseIntPipe({ optional: true }))
@@ -88,29 +32,24 @@ export class PoolController {
       ? {
           OR: [
             {
-              name: {
+              token0Symbol: {
                 contains: searchValue,
                 mode: Prisma.QueryMode.insensitive,
               },
             },
             {
-              symbol: {
+              token1Symbol: {
                 contains: searchValue,
                 mode: Prisma.QueryMode.insensitive,
               },
             },
           ],
-          status: TokenState.IN_POOL,
         }
-      : { status: TokenState.IN_POOL };
+      : {};
 
-    const count = await this.db.token.count({ where: searchFilter });
-    console.log(page, limit, skip, searchFilter);
-    const tokens = await this.db.token.findMany({
+    const count = await this.db.poolStats.count({ where: searchFilter });
+    const pools = await this.db.poolStats.findMany({
       where: searchFilter,
-      include: {
-        TokenPrice: true,
-      },
       take: limit,
       skip: skip,
     });
@@ -119,7 +58,7 @@ export class PoolController {
     const hasPreviousPage = page > 1;
 
     return {
-      data: tokens,
+      data: pools,
       pagination: {
         currentPage: page,
         itemsPerPage: limit,
@@ -129,5 +68,17 @@ export class PoolController {
         hasPreviousPage: hasPreviousPage,
       },
     };
+  }
+
+  @Get('/top')
+  @CacheKey('pool:top')
+  @CacheTTL(5 * 60 * 1000)
+  async getTopPools() {
+    return await this.db.poolStats.findMany({
+      orderBy: {
+        tvlUSD: 'desc',
+      },
+      take: 4,
+    });
   }
 }
